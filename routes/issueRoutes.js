2007b0e8775626d247fa1e31a60d00628e88fdc1
md sumon mia <engineercse19@gmail.com>
2025-12-07 07:44:46 +0600

    Issue Routes finish

--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import { auth } from "../middleware/auth.js";
+import { createIssue, getAllIssues, getIssueById, upvote } from "../controllers/issueController.js";
+
+const router = express.Router();
+
+router.get("/", getAllIssues);
+router.get("/:id", auth, getIssueById);
+router.post("/", auth, createIssue);
+router.post("/upvote/:id", auth, upvote);
+
+export default router;
 
